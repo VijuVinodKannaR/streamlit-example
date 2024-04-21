b6302516cde270731d1fe0bd144c5c58fcb9afbd
VijuVinodKannaR <167547825+VijuVinodKannaR@users.noreply.github.com>
2024-04-21 22:30:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,37 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import subprocess
 
-"""
-# Welcome to Streamlit!
+def google_chrome():
+    try:
+        # Execute the PowerShell command to upgrade the application as admin
+        subprocess.run(["winget", "upgrade", "Chrome", "--disable-interactivity"], check=True)
+        st.success("Application updated successfully.")
+    except subprocess.CalledProcessError as e:
+        st.error(f"No New Updates Available")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def microsoft_edge():
+    try:
+        # Execute the PowerShell command to upgrade the application as admin
+        subprocess.run(["winget", "upgrade", "Edge", "--disable-interactivity"], check=True)
+        st.success("Application updated successfully.")
+    except subprocess.CalledProcessError as e:
+        st.error(f"No New Updates Available")
+def microsoft_teams():
+    try:
+        # Execute the PowerShell command to upgrade the application as admin
+        subprocess.run(["winget", "upgrade", "Teams", "--disable-interactivity"], check=True)
+        st.success("Application updated successfully.")
+    except subprocess.CalledProcessError as e:
+        st.error(f"No New Updates Available")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Streamlit app setup
+st.title("Applicaiton Update Manager")
+st.write("Click on the below buttons to update your application")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+if st.button("Google Chrome"):
+    google_chrome()
+if st.button("Microsoft Edge"):
+    microsoft_edge()
+if st.button("Microsoft Teams"):
+    microsoft_teams()
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
